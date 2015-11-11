582d7dba4bf25c590e6e6f063d60b34115ab5999
script-deployer <cary.fitzhugh@gmail.com>
2015-11-11 17:47:50 -0500

    Upload shows s3 url

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,2 +1,5 @@
 source scripts/creds
 aws s3 sync target s3://phoenixbot.cnds.io/jars --exclude "*" --include "phoenixbot*standalone.jar"
+
+echo "Update with this:"
+echo https://s3.amazonaws.com/phoenixbot.cnds.io/jars/`ls target/*-standalone.jar`
