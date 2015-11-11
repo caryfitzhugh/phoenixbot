1c0978ca3e81fe736c453ef2c623a20514f88850
script-deployer <cary.fitzhugh@gmail.com>
2015-11-11 17:40:37 -0500

    Version 1.0.42

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject phoenixbot "1.0.42-SNAPSHOT"
+(defproject phoenixbot "1.0.42"
   :description "CN-ATG Workflow tools"
   :url "http://github.com/Ziplist/phoenixbot"
   :min-lein-version "2.0.0"
