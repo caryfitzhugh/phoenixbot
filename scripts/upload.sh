d3059bab0eb062bbcf9070a6ea01a7d5cf6a8feb
script-deployer <cary.fitzhugh@gmail.com>
2015-11-12 10:44:41 -0500

    fixed the loading output

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -2,4 +2,4 @@ source scripts/creds
 aws s3 sync target s3://phoenixbot.cnds.io/jars --exclude "*" --include "phoenixbot*standalone.jar"
 
 echo "Update with this:"
-echo https://s3.amazonaws.com/phoenixbot.cnds.io/jars/`ls target/*-standalone.jar`
+echo https://s3.amazonaws.com/phoenixbot.cnds.io/jars/`cd target && ls *-standalone.jar`
